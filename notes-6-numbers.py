15a185975d2179f9a06de0b5f13993d813e52cbd
Audrey <audchan027@gmail.com>
2025-11-05 13:05:19 -0800

    added numbers notes

---
+++ notes-6-numbers.py
@@ -0,0 +1,18 @@
+# Numbers and Operations
+# Author : Audrey
+# 5 November 2025
+
+# Create an algorithm to gather data to find the most popular bbt places around us
+
+# Version 1
+# Show all bbt choices
+# Ask the user for their choice
+# Add their vote to a running tally
+# Give some raw scores
+# Give score as a percentage
+
+# Version 2
+# Ask the user what their favourite bbt place is
+# Add their vote to a running tally
+# Giive the raw score
+# Give score as percentage
